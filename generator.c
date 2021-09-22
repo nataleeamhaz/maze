2555376d3100e2081833a085299b9976b6e155ab
Natalee Amhaz <natalee_amhaz@brown.edu>
2021-09-21 20:28:58 -0400

    k

--- generator.c
+++ generator.c
@@ -242,11 +242,6 @@ int main(int argc, char **argv) {
         num_rows = atoi(argv[2]);
         num_cols = atoi(argv[3]);
     }
-    if (num_rows <= 0 || num_cols <= 0){
-        fprintf(stderr, "Not greater than 0");
-        return 1;
-    }
-    
     // TODO: implement this function
     struct maze_room newM[num_rows][num_cols];
     initialize_maze(num_rows,num_cols,newM);
@@ -254,4 +249,6 @@ int main(int argc, char **argv) {
     int encodeM[num_rows][num_cols];
     encode_maze(num_rows, num_cols, newM, encodeM);
     write_encoded_maze_to_file(num_rows, num_cols, encodeM, file_name);
+
+    
 }
